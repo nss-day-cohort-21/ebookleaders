d936f72f50140f17db8cfb9b40bab8b5134d7f4a
Jim Vickery <jimrv8@yahoo.com>
2017-01-07 21:29:45 -0600

    add existing files

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
 
-<<<<<<< HEAD
 	devise_for :users , controllers: { confirmations: 'confirmations' }
-=======
-	devise_for :users
->>>>>>> 18a7666091325a4cfa0a27d4f82397656a6f67ae
 	resources :books do
 		resources :reviews
 	end	
