5041dc8ff055495f8bea66a8c2a2a75e926da91c
Jim Vickery <jimrv8@yahoo.com>
2017-01-11 16:40:36 -0600

    update existing files to get raty stars to display

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,8 +1,10 @@
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = '1.0'
 
+#chnaged line 6 to second line 7 in attempt to get raty stars working
+#Rails.application.config.assets.version = '1.0'
+Rails.application.config.assets.precompile += %w( ratyrate.js )
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
