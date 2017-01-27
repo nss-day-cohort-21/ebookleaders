f7f4ddf589604e303215ca4e370fe93c858b1456
Jim Vickery <jimrv8@yahoo.com>
2017-01-26 18:21:27 -0600

    update existing files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ before_action :configure_permitted_parameters, if: :devise_controller?
 
 	def configure_permitted_parameters
 	  devise_parameter_sanitizer.permit(:sign_in, keys: [:name, :email])
-	  devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation]) 
+	  devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :twitter_handle, :email, :password, :password_confirmation]) 
 	  devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :twitter_handle, :author_bio, :reviewer_bio, :password, :password_confirmation, :current_password, :avatar])
 	end
 end
