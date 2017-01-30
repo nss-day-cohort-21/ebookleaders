465299d33d17fe883a6b491ce95fa1d53960e6ad
Jim Vickery <jimrv8@yahoo.com>
2017-01-30 17:12:44 -0600

    update existing files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170127232344) do
+ActiveRecord::Schema.define(version: 20170129231817) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -90,6 +90,8 @@ ActiveRecord::Schema.define(version: 20170127232344) do
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
     t.string   "twitter_handle"
+    t.string   "provider"
+    t.string   "uid"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
---
+++ lib/tasks/twitterposts.rake
@@ -0,0 +1,14 @@
+namespace :bookclubpro
+desc "Automatically posts book content to Twitter"
+task :update_ebookleaders do |variable|
+	require 'json'
+	require 'open-uri'
+
+#post books to twitter
+
+url=  Book.find(5) 
+data = JSON.parse(url)
+
+book = data["book.titleLstring"]
+
+end
