b7fe59deccf676c2634ac13de88a9e96fbdf1614
Jim Vickery <jimrv8@yahoo.com>
2017-02-03 17:17:59 -0600

    update existing files, added last nominate day

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170129231817) do
+ActiveRecord::Schema.define(version: 20170203224423) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20170129231817) do
     t.string   "book_teaser_content_type"
     t.integer  "book_teaser_file_size"
     t.datetime "book_teaser_updated_at"
+    t.date     "last_nominate_day"
   end
 
   create_table "bots", force: :cascade do |t|
