2188cd0279c51a70f240f76a36db938eac1b3a7b
Jim Vickery <jimrv8@yahoo.com>
2018-02-05 17:29:57 -0600

    Removed migration and added book image (with typo) column back to original migration.

--- db/migrate/20161228065624_create_books.rb
+++ db/migrate/20161228065624_create_books.rb
@@ -3,6 +3,7 @@ class CreateBooks < ActiveRecord::Migration[5.0]
     create_table :books do |t|
       t.string :titleLstring
       t.string :details
+      t.string :book_imgage
       t.string :author
       t.timestamps
     end
