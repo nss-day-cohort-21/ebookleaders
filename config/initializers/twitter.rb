6806cb055519cc317f29ab71d043425e936a9725
Jim Vickery <jimrv8@yahoo.com>
2018-05-30 16:24:26 -0500

    commented out non used items.

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,10 +1,11 @@
+# These keys are not activated on Twitter.
 
-CLIENT = Twitter::REST::Client.new do |config| 
-config.consumer_key = '0bk66sKarkxNKCWn6aWf6el0G'
-config.consumer_secret = 'cptww1dzeA131HRVCBF6wuTvBHsiCD3J7n1gwd2bdpLO0Ayg5v'
-config.access_token = '818379037370437632-AvoIlEWYFpBI63t3C86xDFxTkZkbf87' 
-config.access_token_secret = 'wHF5hW6i5BZor3AMQCyLqmnjtM6zFmeVMHvvhZpc9H9Ac'
+# CLIENT = Twitter::REST::Client.new do |config| 
+# config.consumer_key = '0bk66sKarkxNKCWn6aWf6el0G'
+# config.consumer_secret = 'cptww1dzeA131HRVCBF6wuTvBHsiCD3J7n1gwd2bdpLO0Ayg5v'
+# config.access_token = '818379037370437632-AvoIlEWYFpBI63t3C86xDFxTkZkbf87' 
+# config.access_token_secret = 'wHF5hW6i5BZor3AMQCyLqmnjtM6zFmeVMHvvhZpc9H9Ac'
 
-end
+# end
 
 
